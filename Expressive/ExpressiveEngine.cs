c809e8a9e42729a16a474236250d751ff89c9c96
ashmind <ashmind@gmail.com>
2011-05-12 23:35:57 +0400

    Removed redundant namespace references.

--- Expressive/ExpressiveEngine.cs
+++ Expressive/ExpressiveEngine.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
-using Expressive.Decompilation;
 using Expressive.Decompilation.Pipelines;
-using Expressive.Decompilation.Steps;
-using Expressive.Decompilation.Steps.Clarity;
-using Expressive.Decompilation.Steps.IndividualElements;
-using Expressive.Decompilation.Steps.StatementInlining;
 
 namespace Expressive {
     public static class ExpressiveEngine {
         public static LambdaExpression ToExpression(MethodBase method) {
-            var decompiler = new Decompiler(new DefaultPipeline());
-            return decompiler.Decompile(method);
+            return new Decompiler(new DefaultPipeline()).Decompile(method);
         }
     }
 }
