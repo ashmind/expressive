7ad00f88b0820c4576afd4a51c4f59a309a8666c
ashmind <ashmind@gmail.com>
2011-05-15 17:14:19 +0400

    More conversion tests.

--- Expressive.Tests/TestClasses/Conversions.cs
+++ Expressive.Tests/TestClasses/Conversions.cs
@@ -4,24 +4,40 @@ using System.Linq;
 
 namespace Expressive.Tests.TestClasses {
     public static class Conversions {
-        private const string Convert = "x => Convert(x)";
+        private const string Explicit = "x => Convert(x)";
+        private const string Implicit = "x => x";
 
-        [ExpectedExpression(Convert)]
-        public static long Int32ToInt64(int x) {
-            return x;
-        }
-
-        [ExpectedExpression(Convert)]
+        [ExpectedExpression(Explicit)]
         public static short Int32ToInt16(int x) {
             return (short)x;
         }
 
-        [ExpectedExpression(Convert)]
+        [ExpectedExpression(Explicit)]
+        public static ushort Int32ToUInt16(int x) {
+            return (ushort)x;
+        }
+
+        [ExpectedExpression(Implicit)]
+        public static uint Int32ToUInt32(int x) {
+            return (uint)x;
+        }
+
+        [ExpectedExpression(Explicit)]
+        public static long Int32ToInt64(int x) {
+            return x;
+        }
+
+        [ExpectedExpression(Implicit)]
+        public static uint Int32ToUInt64(int x) {
+            return (uint)x;
+        }
+
+        [ExpectedExpression(Explicit)]
         public static float Int32ToSingle(int x) {
             return x;
         }
 
-        [ExpectedExpression(Convert)]
+        [ExpectedExpression(Explicit)]
         public static double Int32ToDouble(int x) {
             return x;
         }
